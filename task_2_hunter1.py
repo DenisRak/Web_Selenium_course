4058b059d6fe7fbf4fd133a4dd5cda8981aaa599
Rak Denis <rakdenis32@gmail.com>
2025-02-28 10:02:46 +0300

    create event_choice

--- task_2_hunter1.py
+++ task_2_hunter1.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 
-with webdriver.Chrome(service=Service(ChromeDriverManager().install())) as driver:
+with webdriver.Chrome() as driver:
     driver.get("http://parsinger.ru/selenium/2/2.html")
     driver.find_element(By.LINK_TEXT, "16243162441624").click()
     res = driver.find_element("xpath","//p[@id='result']")
