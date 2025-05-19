4fa907cec4b909009c84adca1d54514d56ca981d
Rak Denis <rakdenis32@gmail.com>
2025-05-19 10:54:17 +0300

    add 8_4_3

--- task_8_4_3.py
+++ task_8_4_3.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import re
 
+
 with webdriver.Chrome() as browser:
     browser.get('https://parsinger.ru/selenium/8/8.4.1/')
     element = browser.find_element(By.TAG_NAME, 'iframe')
